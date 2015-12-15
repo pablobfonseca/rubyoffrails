32c31699683367acf43d57acacfa54635ba53009
Pablo Fonseca <pablofonseca777@gmail.com>
2015-12-14 22:07:45 -0200

    Updated Tiger Level

--- sinatraweb/homework/application.rb
+++ sinatraweb/homework/application.rb
@@ -3,8 +3,14 @@ require 'bundler/setup'
 require 'sinatra'
 require 'shotgun'
 
+enable :sessions
+
 get '/' do
-  things = %w(programming music games football basketball)
-  @random_word = things.sample
+  @about_me = [
+    { title: "I love coding", content: "I code when I am sad or happy, doesn't matter :D " },
+    { title: "I love learning", content: "Doesn't matter the subject, the thing is to learn" },
+    { title: "Cats", content: "I have two cats, Mike and Jack" },
+    { title: "Speak english", content: "If I could forget my native language and just speak english, It'll be cool!" }
+  ]
   erb :about
 end
